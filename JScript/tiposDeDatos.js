349f852ff4a0986cf4f85bec857c39701433b25b
Miguel Alejandro Lescano <alescano.test@gmail.com>
2023-02-19 23:11:57 -0300

    Funciones para tipos de datos

---
+++ JScript/tiposDeDatos.js
@@ -0,0 +1,48 @@
+//? Doble barra
+//Comentario de una linea
+
+//? Abir barra + asterisco
+/*
+Comentario
+de
+varias
+lineas
+*/
+
+//! Loguear mensaje en consola
+console.log("Hola mundo!")
+
+
+//?Tipos de datos:
+//! undefined, null, boolean, string, symbol, number y object.
+
+var a = "String"; //String
+var x = "String con \"comillas\"" //String con comillas dobles
+var y = 'String con "comillas"' //String con comillas dobles
+var b = 8;  //Entero
+var c = 10.2; // Doble
+var w = true;
+var x; // Undefined
+
+
+console.log(a, x, y, b, c, x, w)
+
+//! Secuencias de escape
+
+//* Código | Resultado
+//*  \'     Comilla simple
+//*  \"     Comilla doble
+//*  \\     Barra invertida
+//*  \n     Linea nueva
+//*  \r     Retorno de carro
+//*  \t     Tabulación
+//*  \b     Retroceso
+//*  \f     Salto de página
+//* ...
+
+//! Concatenación
+
+var verbo = "programar"
+var mensaje = "Estoy aprendiendo a " + verbo
+console.log(mensaje)
+
