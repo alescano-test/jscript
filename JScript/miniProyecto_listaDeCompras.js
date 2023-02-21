924c2df4d61aa4ff1f5a0ef6bcb6f4b8ee709337
Miguel Alejandro Lescano <alescano.test@gmail.com>
2023-02-21 17:47:38 -0300

    hecho

---
+++ JScript/miniProyecto_listaDeCompras.js
@@ -0,0 +1,5 @@
+var miListaDeCompras = [["Cereal", 2],["Lecha", 2],["Galletas", 4],["Pan", 5], ["Refresco", 7], ["Pollo", 7]]
+
+console.log("Voy a comprar: "+miListaDeCompras[0][1]+" cajas de "+miListaDeCompras[0][0], 
+"\nVoy a comprar: "+miListaDeCompras[1][1]+" cajas de "+miListaDeCompras[1][0],  
+"\nVoy a comprar: "+miListaDeCompras[2][1]+" cajas de "+miListaDeCompras[2][0]);
