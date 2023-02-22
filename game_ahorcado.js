73a7d10f8e7594d2422f47294b4a08b5c3c75a12
Miguel Alejandro Lescano <alescano.test@gmail.com>
2023-02-22 18:51:44 -0300

    comienzo juego ahorcado

---
+++ game_ahorcado.js
@@ -0,0 +1,7 @@
+//! Juego ahorcado
+
+//* Definimos las palabras que van a jugarse
+var palabrasEnJuego = ["oviparo", "botella", "zunilda", "kamehameha", "bazuka", "japones"];
+
+var valor = prompt("Hola, ingrese un valor");//prompt();
+
